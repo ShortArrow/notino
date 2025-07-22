8f30258246cac8dbf9896241bfa18263962d9d43
ShortArrow <bamboogeneral@shortarrow.jp>
2025-07-22 18:21:47 +0900

    Add blinking LED feature to notino.ino

--- notino.ino
+++ notino.ino
@@ -1,4 +1,9 @@
 const int ledPin = 13;
+bool notification = false;
+
+unsigned long previousMillis = 0;
+const long interval = 100;
+int ledState = LOW;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -8,13 +13,32 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     char command = Serial.read();
-
     if (command == 'H') {
-      digitalWrite(ledPin, HIGH);
-      Serial.println("LED ON");
+      notification = true;
+      Serial.println("HIGH");
+      previousMillis = millis(); // Reset the timer when notification starts
     } else if (command == 'L') {
-      digitalWrite(ledPin, LOW);
-      Serial.println("LED OFF");
+      notification = false;
+      Serial.println("LOW");
     }
   }
+
+  if (notification) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      // Toggle the LED state
+      if (ledState == LOW) {
+        ledState = HIGH;
+      } else {
+        ledState = LOW;
+      }
+    }
+  }
+  else {
+    // Off notification, turn off the LED
+    ledState = LOW;
+  }
+
+  digitalWrite(ledPin, ledState);
 }
